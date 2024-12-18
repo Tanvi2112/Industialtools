df8a4f185e0d0fb6cc9d3c590cefea3417382a72
Tanvi2112 <kindretanvi62@gmail.com>
2024-12-18 12:34:43 -0800

    new statements are added

--- helloworld.java
+++ helloworld.java
@@ -1,6 +1,7 @@
-public class helloworld{
-public static void main(String args[])
-{
-System.out.print("hello world");
-}
+public class helloworld {
+    public static void main(String args[]) {
+        System.out.print("hello world");
+        System.out.println("tanvi");
+        System.out.println("happy new year");
+    }
 }
